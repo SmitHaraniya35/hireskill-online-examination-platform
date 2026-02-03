c802c0b373971f644da9012c7df84262d7f201f9
Om Patel <patel.om.manubhai@gmail.com>
2026-02-03 15:30:24 +0530

    Integrate all routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,30 @@
+import { Route, Routes, Navigate } from 'react-router-dom'
 import './App.css'
+import Login from './pages/admin/Login.tsx'
+import ForgetPassword from './pages/admin/ForgetPassword.tsx'
+import ResetPassword from './pages/admin/ResetPassword.tsx'
+import NotFound from './components/NotFound.tsx'
+import Dashboard from './pages/admin/Dashboard.tsx'
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-      <h1>Hireskill Online Examination Platform</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="admin/login" />} />
+
+      <Route path="/admin/login" element={<Login />} />
+      <Route path="/admin/forget-password" element={<ForgetPassword />} />
+      <Route path="/admin/reset-password" element={<ResetPassword />} />
+      <Route 
+        path="/admin/dashboard"
+        element={
+          // <ProtectedRoute>
+            <Dashboard />
+          /* </ProtectedRoute> */
+        }
+      />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
   )
 }
 
-export default App
+export default App;
