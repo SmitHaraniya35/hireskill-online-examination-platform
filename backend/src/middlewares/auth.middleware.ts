89a1e557eeb2f2b3bc2dfac4c7767792f1ab12bd
SmitHaraniya35 <smeetharaniya@gmail.com>
2026-02-04 10:41:04 +0530

    feat: Add auth middleware

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,1 +1,21 @@
-export const authMiddleware = () => {}
+import type { Request, Response, NextFunction } from "express";
+import { verifyAccessToken } from "../utils/jwt.utils.ts";
+import { ERROR_MESSAGES } from "../constants/index.ts";
+import type { AuthJwtPayload } from "../types/controller/index.ts";
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const accessToken = req.headers.authorization?.split(" ")[1];
+
+  if (!accessToken) {
+    return res.unauthorized(ERROR_MESSAGES.ACCESS_TOKEN_MISSING);
+  }
+  
+  try {
+    const data = verifyAccessToken(accessToken) as AuthJwtPayload;
+    console.log("Protect middleware - verified token data:", data);
+    req.user = data;
+    next();
+  } catch {
+    return res.unauthorized(ERROR_MESSAGES.ACCESS_TOKEN_INVALID);
+  }
+};
