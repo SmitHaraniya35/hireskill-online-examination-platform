903ff215b202176bd990c95b680319ca52851a98
Om Patel <patel.om.manubhai@gmail.com>
2026-02-10 17:53:15 +0530

    Add some routes related to the user and test page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,16 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import {AuthProvider} from "../src/context/AuthContext.tsx";
+import {AdminAuthProvider} from "./context/AdminAuthContext.tsx";
+import {UserAuthProvider} from "./context/UserAuthContext.tsx";
 import "react-datepicker/dist/react-datepicker.css";
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <AdminAuthProvider>
+        <UserAuthProvider>
+          <App />
+        </UserAuthProvider>
+      </AdminAuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
