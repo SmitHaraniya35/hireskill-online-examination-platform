383250e5f0dcb343be593c07a91f9d8ae4b5009d
Om Patel <patel.om.manubhai@gmail.com>
2026-02-13 09:43:47 +0530

    Temp add the component to verify

--- frontend/src/pages/admin/ViewTestDetails.tsx
+++ frontend/src/pages/admin/ViewTestDetails.tsx
@@ -1,11 +1,15 @@
 
 
-// const ViewTestDetails: React.FC = () => {
-//     return (
-//         <div>
+const ViewTestDetails: React.FC = () => {
+    return (
+        <div>
+            <h2 className="text-2xl font-bold mb-4">Test Details</h2> 
+            <h2>list of student name</h2>     
+            <h2>student email</h2>
+            <h1>student asssigned problem id</h1>
+            <h2>Student Status(active or not)</h2>
+        </div>
+    )
+}
 
-//         </div>
-//     )
-// }
-
-// return ViewTestDetails;
+export default ViewTestDetails;
