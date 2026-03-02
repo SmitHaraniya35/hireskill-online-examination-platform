9b68954055e06cedb27b6c25b739e6dddf9823ff
SmitHaraniya35 <smeetharaniya@gmail.com>
2026-03-02 11:27:23 +0530

    refactor: refactoring routes of studentAttempt

--- backend/src/routes/studentAttempt.routes.ts
+++ backend/src/routes/studentAttempt.routes.ts
@@ -8,11 +8,12 @@ import { ValidateStudentAttemptSchema, CreateStudentAttemptSchema } from "../val
 const router = express();
 
 router.post("/create-student-attempt", validateRequest(CreateStudentAttemptSchema), StudentAttemptController.createStudentAttempt);
-router.delete("/delete-student-attempt/:id", authMiddleware, validateRequest(IdSchema), StudentAttemptController.deleteStudentAttempt);
+router.get("/get-student-attempt/:id", validateRequest(IdSchema), StudentAttemptController.getStudentAttemptById);
 router.get("/get-student-attempts-details/:testId", authMiddleware, validateRequest(), StudentAttemptController.getStudentAttemptsDetailsByTestId);
 router.put("/submit-student-attempt/:id", validateRequest(IdSchema), StudentAttemptController.submitStudentAttempt);
+router.delete("/delete-student-attempt/:id", authMiddleware, validateRequest(IdSchema), StudentAttemptController.deleteStudentAttempt);
+
 router.get("/:id/get-problem-id", validateRequest(IdSchema), StudentAttemptController.validateStudentAttemptAndGetCodingProblemId);
-router.get("/get-student-attempt/:id", validateRequest(IdSchema), StudentAttemptController.getStudentAttemptById);
 router.post("/validate-student-attempt", validateRequest(ValidateStudentAttemptSchema), StudentAttemptController.validateStudentAttemptByEmail);
 
 export default router;
