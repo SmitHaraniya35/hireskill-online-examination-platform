1f3060018f88a5116d952c710c1c44d03eb220cf
Om Patel <patel.om.manubhai@gmail.com>
2026-03-12 16:51:23 +0530

    feat: add service related to the testCase

---
+++ frontend/src/services/testCase.services.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import type { axiosResponse } from "../types/index.types";
+
+const API_URL = `${import.meta.env.VITE_BACKEND_API_URL}/test-case`;
+
+const getAuthHeaders = () => {
+  const token = localStorage.getItem("admin_token");
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "ngrok-skip-browser-warning": "69420",
+    },
+    withCredentials: true,
+  };
+};
+
+const testCaseService = {
+  deleteTestCase: async (id: string) => {
+    const response = await axios.delete<axiosResponse>(
+      `${API_URL}/delete-test-case/${id}`,
+      getAuthHeaders(),
+    );
+    return response.data;
+  },
+};
+export default testCaseService;
