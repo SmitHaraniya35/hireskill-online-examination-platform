0acf95a1ed711685d62f0529dee0941c530fbdb9
Om Patel <patel.om.manubhai@gmail.com>
2026-03-12 09:55:42 +0530

    fix: update routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import NotFound from './components/shared/NotFound.tsx'
 import VerifyOtp from './features/auth/admin/VerifyOtp.tsx'
 import TestLinkManager from './features/test/handleTest/TestsManager.tsx'
 import CodingProblem from './features/problems/CodingProblems.tsx'
-import AddNewProblem from './features/problems/AddNewProblem.tsx'
+import CreateCodingProblem from './features/problems/CreateCodingProblem.tsx'
 import LandingPage from './components/LandingPage.tsx'
 import AdminRoutes from './routes/AdminRoutes.tsx'
 import TestEntry from './features/test/CodingPlatform/TestStartPage.tsx'
@@ -18,6 +18,7 @@ import StudentManagementLayout from './features/student/StudentManagementLayout.
 import TestCompletePage from './features/test/CodingPlatform/TestCompletePage.tsx'
 import AdminDashboardLayout from './features/Dashboard/AdminDashboardLayout.tsx'
 import SubmissionDetails from './features/test/handleTest/SubmissionDetails.tsx'
+import CreateNewTest from './features/test/handleTest/CreateNewTest.tsx'
 
 const App: React.FC = () => {
   return (
@@ -31,23 +32,25 @@ const App: React.FC = () => {
         <Route path="/admin/verify-otp" element={<VerifyOtp />} />
         <Route path="/admin/reset-password/" element={<ResetPassword />} />
         
-        <Route element={<AdminRoutes/>}>
+        {/* <Route element={<AdminRoutes/>}> */}
           <Route element={<AdminLayout/>}>
             <Route path="/admin/dashboard" element={<AdminDashboardLayout />}/>
             <Route path="/admin/create-exam" element={<TestLinkManager/>} />
+            <Route path="/admin/create-exam/generate-new-test" element={<CreateNewTest/>} />
             <Route path="/admin/coding-problem" element={<CodingProblem/>}/>
-            <Route path="/admin/coding-problem/add-new-problem"
+            <Route path="/admin/coding-problem/create-coding-problem" element={<CreateCodingProblem />} />
+            {/* <Route path="/admin/coding-problem/add-new-problem"
               element={
                 <AddNewProblem
                   closeModal={() => {}}
                   refreshLinks={() => {}}
                 />
               }
-            />
+            /> */}
             <Route path="/admin/student-management" element={<StudentManagementLayout/>}/>
             <Route path="/submission/:id" element={<SubmissionDetails />} />
           </Route>
-        </Route>
+        {/* </Route> */}
         
         
         {/* Candidate test flow */}
