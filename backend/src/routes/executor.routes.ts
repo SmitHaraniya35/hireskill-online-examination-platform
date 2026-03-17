efc9e213665b51004b9504adf090738f8f59a0b4
SmitHaraniya35 <smeetharaniya@gmail.com>
2026-03-17 18:45:49 +0530

    feat: add runCode and submitCode schema

--- backend/src/routes/executor.routes.ts
+++ backend/src/routes/executor.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { validateRequest } from "../validators/request.validator.ts";
 import * as executorController from "../controllers/executor.controller.ts";
+import { RunCodeSchema, SubmitCodeSchema } from "../validators/executor.schema.ts";
 
 const router = express();
 
-router.post("/run", validateRequest(), executorController.runCode);
-router.post("/submit", validateRequest(), executorController.submitCode);
+router.post("/run", validateRequest(RunCodeSchema), executorController.runCode);
+router.post("/submit", validateRequest(SubmitCodeSchema), executorController.submitCode);
 
 export default router;
