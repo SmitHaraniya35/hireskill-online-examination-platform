d093b2be94554ae44926635b00cf196b61451afb
Om Patel <patel.om.manubhai@gmail.com>
2026-03-20 16:43:35 +0530

    refactor: update types.

--- frontend/src/types/student.types.ts
+++ frontend/src/types/student.types.ts
@@ -2,11 +2,11 @@ export interface Student{
     id?: string;
     name: string;
     email: string;
-    phone: string;
+    phone: number;
     college: string;
     degree?: string;
     branch?: string;
-    graduation_year?: string;
+    graduation_year?: number;
     skills?: string;
     resume_url?: string;
     linkedin_url?: string;
--- frontend/src/types/submission.types.ts
+++ frontend/src/types/submission.types.ts
@@ -36,6 +36,28 @@ export interface SubmitResponse {
     executionMappingList: ExecutionMappingList[]
 }
 
+export interface Submission {
+    id: string;
+    assigned_problem_id: string;
+    total_test_cases: number;
+    passed_test_cases: number;
+    language: string;
+    source_code: string;
+    submitted_at: string;
+    status: string;
+    execution_time: string;
+    memory_used: string;
+}
+
+export interface Problem {
+    id: string;
+    title: string;
+    difficulty: string;
+    problem_description: string;
+    topic: string[];
+}
+
 export interface GetSubmissionResponse {
-    data: RunResponse;
+    problem: Problem;
+    submission: Submission;
 }
