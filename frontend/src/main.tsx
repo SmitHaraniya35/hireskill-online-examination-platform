54f9faaa366ddeda94f30d1a1c971813877c914b
Om Patel <patel.om.manubhai@gmail.com>
2026-03-30 14:59:50 +0530

    update: main file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import { AdminAuthProvider } from "./context/authContext.tsx";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  // <StrictMode>
     <BrowserRouter>
-      <AdminAuthProvider>
         <App />
         {/* Global Toast Notification */}
         <ToastContainer
@@ -23,7 +21,6 @@ createRoot(document.getElementById("root")!).render(
           draggable
           theme="light"
         />
-      </AdminAuthProvider>
     </BrowserRouter>
-  </StrictMode>
+  // </StrictMode>
 );
