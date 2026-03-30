d64c259e01303c02932ede6e9b13371e6bf95924
Om Patel <patel.om.manubhai@gmail.com>
2026-03-30 18:37:46 +0530

    fix: some logic in admin routes.

--- frontend/src/routes/AdminRoutes.tsx
+++ frontend/src/routes/AdminRoutes.tsx
@@ -1,5 +1,5 @@
 import {Navigate, Outlet } from "react-router-dom";
-import { AdminAuthProvider, useAuth } from "../context/authContext"
+import { useAuth } from "../context/authContext"
 
 
 const AdminRoutes: React.FC = () => {
