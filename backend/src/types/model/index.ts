19e1ef7790e25f4f7d28663a3f2bf0db6994a55c
SmitHaraniya35 <smeetharaniya@gmail.com>
2026-04-13 13:00:48 +0530

    refactor: update BaseModel

--- backend/src/types/model/index.ts
+++ backend/src/types/model/index.ts
@@ -5,6 +5,7 @@ import type {
   QueryFilter,
   UpdateQuery,
   Query,
+  UpdateWriteOpResult
 } from "mongoose";
 
 export interface GenerateSchemaFunction {
@@ -26,10 +27,10 @@ export interface BaseModel<T> extends Model<T> {
   findActive(filter?: QueryFilter<T>, filterOptions?: ProjectionType<T>): Query<T[], T>;
   findOneActive(filter: QueryFilter<T>, filterOptions?: ProjectionType<T>): Query<T | null, T>;
   findByIdActive(id: string, filterOptions?: ProjectionType<T>): Query<T | null, T>;
-  updateOneByFilter(filter: QueryFilter<T>, update: UpdateQuery<T>): any;
-  updateManyByFilter(filter: QueryFilter<T>, update: UpdateQuery<T>): any;
-  softDelete(filter: QueryFilter<T>): any;
-  softDeleteMany(filter: QueryFilter<T>): any;
-  restore(filter: QueryFilter<T>): any;
-  hardDelete(filter: QueryFilter<T>): any;
+  updateOneByFilter(filter: QueryFilter<T>, update: UpdateQuery<T>): Query<UpdateWriteOpResult, T>;
+  updateManyByFilter(filter: QueryFilter<T>, update: UpdateQuery<T>): Query<UpdateWriteOpResult, T>;
+  softDelete(filter: QueryFilter<T>): Query<UpdateWriteOpResult, T>;
+  softDeleteMany(filter: QueryFilter<T>): Query<UpdateWriteOpResult, T>;
+  restore(filter: QueryFilter<T>): Query<UpdateWriteOpResult, T>;
+  hardDelete(filter: QueryFilter<T>): Query<any, T>;
 }
